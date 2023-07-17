b0fc1490dd94dfb9e1547cabeecea078f61da828
student <student@gnu.ac.in>
2023-07-17 10:42:31 +0530

    commited

--- src/main/kotlin/PRACTICAL1/1.kt
+++ src/main/kotlin/PRACTICAL1/1.kt
@@ -1,3 +1,12 @@
 fun main(){
-    print("hello")
+    var a:Int =10
+    var b:String="parth"
+    var c:Boolean=true
+    var d:Float=1.11f
+    var e:Char='a'
+    var f:Double=111.222
+    var g:Byte=101
+
+    println("$a is integer \n $b is string \n$c is boolean \n$d is float\n$e is character\n$f is bouble \n$g is byte")
+
 }
