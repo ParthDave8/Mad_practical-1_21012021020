8ba7cf8c933737cdbf18c1804800acb2580a7a49
student <student@gnu.ac.in>
2023-07-24 10:36:04 +0530

    classes

---
+++ src/main/kotlin/PRACTICAL1/classes.kt
@@ -0,0 +1,14 @@
+class Students(var eeno:Int){
+    var name:String=""
+    constructor(n:String):this(20){
+        name = n
+
+    }
+}
+class person()
+fun main(){
+    val a = Students("parth")
+
+    println(a.name)
+    println(a.eeno)
+}
