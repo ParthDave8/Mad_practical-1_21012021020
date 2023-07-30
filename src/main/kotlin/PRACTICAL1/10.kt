07cce74b6e2693d3cd0a7e33bee2782ef69456d6
parth <daveparth025@gmail.com>
2023-07-30 21:40:37 +0530

    updated

--- src/main/kotlin/PRACTICAL1/10.kt
+++ src/main/kotlin/PRACTICAL1/10.kt
@@ -1,35 +1,48 @@
-class Car(
-        val type: String,
-        val model: String,
-        val price: Double,
-        val owner: String,
-        var KmDrive: Int
-) {
-    fun getOriginalCarPrice(): Double {
-        return price
+class Car(type:String, model:Int, price: Double, owner:String, milesDrive: Double){
+    var carType=type;
+    val carModel=model;
+    val carPrice=price;
+    var carOwner=owner;
+    val carMilesDrive=milesDrive;
+    init{
+        println("Object of class is created and Init is called.");
     }
-    fun getCurrentCarPrice(): Double {
-        val depreciationRate = 0.1 // 10% depreciation per 10,000 miles
-        val depreciationFactor = (KmDrive / 10000) * depreciationRate
-        return price - (price * depreciationFactor)
+    fun getCarInformation(){
+        println("Car Information : ${carType} , ${carModel}");
+        println("Car Owner : ${carOwner}");
+        println("Miles Drive : ${carMilesDrive}");
     }
-    fun displayCarInfo() {
-        println("Car Information:")
-        println("Type: $type")
-        println("Model: $model")
-        println("Original Price: ${getOriginalCarPrice()}")
-        println("Current Price: ${getCurrentCarPrice()}")
-        println("Owner: $owner")
-        println("Miles Driven: $KmDrive\n\n")
+
+    fun getOriginalCarPrice():Double{
+        return carPrice;
+    }
+
+    fun getCurrentCarPrice():Double{
+        return carPrice-(carPrice*0.1);
+    }
+    fun displayCarInfo(){
+        println("------------------------");
+        getCarInformation();
+        println("Original Car Price : ${getOriginalCarPrice()}");
+        println("Current Car Price : ${getCurrentCarPrice()}");
+        println("------------------------");
     }
 }
+fun main()
+{
+    println("Creating Car class Object car1 in next line.");
+    var c1=Car("Rr",2018,500000.0,"Parth",36420.0);
+    c1.displayCarInfo();
+    println("Creating Car class Object car2 in next line.");
+    var c2=Car("Pagani",2010,200000.0,"kenil",100080.0);
+    c2.displayCarInfo();
 
-fun main() {
-    val car = Car("SUV", "Jeep Wrangler", 30000.0, "Parth dave", 15000)
-    car.displayCarInfo()
+    println("************ ArrayList of Car *************")
+    var l= ArrayList<Car>();
+    l.add(Car("Audi",2015,3000000.0,"om",5069.10))
 
-    println("Car Type: ${car.type}")
-    println("Car Model: ${car.model}")
-    println("Car Owner: ${car.owner}")
-    println("Miles Driven: ${car.KmDrive}")
+    l.add(Car("Hyundai",2023,600000.0,"Ronan",1005.23))
+    for(i in l){
+        i.displayCarInfo();
+    }
 }
--- src/main/kotlin/PRACTICAL1/8.kt
+++ src/main/kotlin/PRACTICAL1/8.kt
@@ -1,50 +1,36 @@
-fun main() {
-    val x= arrayOf(1,2,3)
-    println("Using arrayOf method: "+x.contentToString())
-
-    val y= Array<Int>(5) {0}
-    println("Using Array<>() method: "+y.contentToString())
-
-    val z= Array<Int>(5) {i: Int -> i }
-    println("Using lambda function: "+z.contentToString())
-
-    val m= IntArray(5){3}
-    val arrAsStrin = m.joinToString (" ")
-    println("Using IntArray method: "+arrAsStrin)
-
-    val b: IntArray = intArrayOf(1,2,3,4,5)
-    val arrAsString = b.joinToString(" ")
-    println("Using intArrayOf method: "+arrAsString)
-
-        val inputArray = arrayOf(
-                intArrayOf(1, 3),
-                intArrayOf(4, 5),
-                intArrayOf(6, 7)
-        )
-
-        val a = IntArray(6)
-
-
-        // Prompt the user to enter values for each index
-        for (i in 1..4) {
-            print("a[$i] = ")
-            val input = readLine()?.toIntOrNull()
-            if (input != null) {
-                a[i] = input
-            } else {
-                println("Invalid input. Please enter a valid integer.")
-                return
+fun main(){
+    var a2= arrayOf(5,4,3,2,1)
+    var a3= intArrayOf(8,9,7)
+    var a4=Array(5){0}
+    println("creste Array using arrayof=${a2.contentToString()}\n")
+    println("creste Array using intarrayof=${a3.contentToString()} \n")
+    println("creste Array using Arry=${a4.contentToString()}\n\n")
+    var a1= arrayListOf<Int>()
+    print("Enter your array size=")
+    var size= readln().toInt()
+    println("enter your $size elements!!")
+    for(i in 0..(size-1)){
+        print("enter a[$i]=")
+        a1.add(readln().toInt())
+    }
+    println("Before sorting array is")
+    a1.forEach{print(it,)}
+    println("\nafter sorting using build-in function\n")
+    a1.sort()
+    a1.forEach{print(it,)}
+    var a5= arrayOf(5,4,3,2,1)
+    println("\nBefore sorting array is\n")
+    a5.forEach { print(it,) }
+    var temp:Int
+    for(i in 0 .. size-1){
+        for(j in 0 .. i){
+            if(a5[i]<a5[j]){
+                temp=a5[i]
+                a5[i]=a5[j]
+                a5[j]=temp
             }
         }
-
-        // Populate the rest of the array using the inputArray values
-        a[0] = inputArray[0][0] + inputArray[0][1]
-        a[5] = inputArray[2][0] - inputArray[1][1]
-
-        // Print the entered array
-        println("Entered Array:")
-        println(a.joinToString(", "))
     }
-
-
-
+    println("\nafter sorting without using build-in function\n")
+    a5.forEach { print(it,) }
+}
